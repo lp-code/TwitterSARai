afb4d7dacb0b79eacced84ad9309375d938bed03
lp-code <lp-code@users.noreply.github.com>
2020-02-29 22:44:55 +0100

    Add script for processing storage table format into xls.

---
+++ data/external/convert_az_table_to_xls.py
@@ -0,0 +1,26 @@
+"""
+Simple conversion from the csv format exported from table storage (through
+storage explorer) to an Excel file with the right columns to copy into the
+main data file.
+
+Enter the working directory and file names below.
+"""
+
+import pandas as pd
+from pathlib import Path
+
+working_dir = Path("c:\\Users\\")
+input_file = "tweets.csv"
+output_file = "tweets.xlsx"
+
+df = pd.read_csv(working_dir / input_file)
+
+df["RowKey"] = df["RowKey"].astype(str)
+df["empty"] = ""
+
+df.to_excel(
+    working_dir / output_file,
+    index=False,
+    columns=["empty", "Score", "RowKey", "FullText", "ScoreML", "empty", "CreatedAt"]
+)
+
